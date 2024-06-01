1406b3fb8c14e9883151bd70540affca010f1ba5
Leonardo Brugnano <lebrug504@gmail.com>
2024-06-01 17:16:01 +0200

    Fixed SUID bit check

--- src/core/rls_server_checks.c
+++ src/core/rls_server_checks.c
@@ -31,13 +31,14 @@ rls_server_checks(void)
     uid_t ruid, euid, suid;
 
     getresuid(&ruid, &euid, &suid);
+    printf("%d\t%d\t%d\n", ruid, euid, suid);
 
     // real uid must NOT be root
     if (ruid == 0)
         fun_fail("Running as root is unnacessary and hence not allowed.");
 
     // program must have SETUID bit set
-    if (euid == 0 || suid == 0)
+    if (euid != 0 || suid != 0)
         fun_fail("Program not configured properly. Please uninstall and reinstall.");
     
     return 1;
