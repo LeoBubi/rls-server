e18650e17af0fb4d94a1190faf8b7bb3aeba6229
Leonardo Brugnano <lebrug504@gmail.com>
2024-06-01 10:25:48 +0200

    Implemented shell part in rls_handler()

--- src/core/rls_handler.c
+++ src/core/rls_handler.c
@@ -36,35 +36,58 @@ rls_handler(void)
         close(client_socket);
         exit(EXIT_FAILURE);
     }
+    
+    // check message type
     if (type != TXTMSG) {
         sndack(client_socket, 40);
         free(username);
         close(client_socket);
         exit(EXIT_SUCCESS);
     }
-    if (strcmp(username, "root") == 0) {    // root user not allowed
+    // root user not allowed
+    if (strcmp(username, "root") == 0) {
         sndack(client_socket, 40);
         free(username);
         close(client_socket);
         exit(EXIT_SUCCESS);
     }
 
-    /* ----- get user shadow record ----- */
+    // get user shadow record
 
     seteuid(0);     // gain root privileges
 
-    struct spwd *user = getspnam(username);
+    struct spwd *sp = getspnam(username);
 
     seteuid(getuid());  // drop root privileges
 
-    if (user == NULL) {
+    free(username);
+
+    if (sp == NULL) {   // user does not exist
         sndack(client_socket, 40);
-        free(username);
         close(client_socket);
         exit(EXIT_SUCCESS);
     }
 
-    free(username);
+    char *hashpass = sp->sp_pwdp;   // save hash password
+
+    // get user passwd record
+
+    struct passwd *pw = getpwnam(sp->sp_namp);
+    if (pw == NULL) {
+        sndack(client_socket, 50);
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // check if user is assigned a shell
+    if (pw->pw_shell == NULL || pw->pw_shell[0] == '\0') {
+        sndack(client_socket, 40);
+        close(client_socket);
+        exit(EXIT_SUCCESS);
+    }
+
+    char *shell = pw->pw_shell;     // save shell
+
     sndack(client_socket, 20);  // username ok
 
     /* ----- receive password ----- */
@@ -85,10 +108,10 @@ rls_handler(void)
         }
 
         // verify password
-        if (type == TXTMSG && strcmp(crypt(password, user->sp_pwdp), user->sp_pwdp) == 0) {
+
+        if (type == TXTMSG && strcmp(crypt(password, hashpass), hashpass) == 0) {
             free(password);
-            sndack(client_socket, 20);  // password ok
-            break;
+            break;  // password correct
         }
 
         free(password);
@@ -97,7 +120,7 @@ rls_handler(void)
 
     /* ----- change uids ----- */
 
-    struct passwd *pw = getpwnam(user->sp_namp);
+    struct passwd *pw = getpwnam(sp->sp_namp);
     if (pw == NULL) {
         sndack(client_socket, 50);
         close(client_socket);
@@ -107,4 +130,62 @@ rls_handler(void)
     setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid);
 
     /* ----- open terminal session ----- */
+
+    int toshell[2];
+    if (pipe(toshell) == -1) {
+        sndack(client_socket, 50);
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    int fromshell[2];
+    if (pipe(fromshell) == -1) {
+        sndack(client_socket, 50);
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t shell_pid = fork();
+    if (shell_pid == -1) {
+        sndack(client_socket, 50);
+        close(client_socket);
+        close(toshell[0]);
+        close(toshell[1]);
+        close(fromshell[0]);
+        close(fromshell[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (shell_pid == 0) /* CHILD */
+    {
+        /* ----- set signal handlers ----- */
+
+        struct sigaction sa;
+        memset(&sa, 0, sizeof(sa));
+        sa.sa_handler = SIG_IGN;
+        sigaction(SIGUSR1, &sa, NULL);
+
+        /* ----- setup file descriptors ----- */
+
+        close(toshell[1]);
+        close(fromshell[0]);
+
+        dup2(toshell[0], STDIN_FILENO);
+        dup2(fromshell[1], STDOUT_FILENO);
+        dup2(fromshell[1], STDERR_FILENO);
+
+        close(toshell[0]);
+        close(fromshell[1]);
+
+        sndack(client_socket, 20);  // terminal session ready to start
+
+        /* ----- execute shell ----- */
+
+        execl(shell, shell, NULL);
+        exit(EXIT_FAILURE);
+    }
+
+    /* HANDLER */
+
+    
 }
