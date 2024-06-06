d7c0c8fc97af71e938f839c8b84ef245ca466037
Leonardo Brugnano <lebrug504@gmail.com>
2024-06-06 09:24:34 +0200

    Reordered included headers

--- include/includes.h
+++ include/includes.h
@@ -5,26 +5,26 @@
 #define _GNU_SOURCE 1
 
 
+#include <arpa/inet.h>
+#include <crypt.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <linux/limits.h>
+#include <netinet/in.h>
+#include <pwd.h>
+#include <shadow.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <linux/limits.h>
-#include <signal.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <shadow.h>
-#include <crypt.h>
-#include <pwd.h>
-#include <sys/select.h>
-#include <errno.h>
-#include <grp.h>
 #include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "defines.h"
 
