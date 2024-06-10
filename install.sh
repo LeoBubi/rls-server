e5610fdc1996f8781c22ffa319098544d86e8293
Leonardo Brugnano <lebrug504@gmail.com>
2024-06-10 10:31:41 +0200

    Updated (un)install scripts

--- install.sh
+++ install.sh
@@ -8,39 +8,129 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 
-# Check if /usr/local/bin exists
-if [ ! -d "/usr/local/bin" ]; then
-    echo "Error: /usr/local/bin directory does not exist"
-    exit 1
-fi
-
-# Check if /usr/local/share/man/ exists
-if [ ! -d "/usr/local/share/man" ]; then
-    echo "Error: /usr/local/share/man directory does not exist"
-    exit 1
-fi
+local bindir="/usr/local/bin"
+local mandir="/usr/local/share/man"
+local srcdir="/usr/local/src"
+local etcdir="/usr/local/etc"
 
 
 # Check if rls-server is installed already
-if [ -f "/usr/local/bin/rls-server" ]; then
+if [ -f "$bindir/rls-server" ]; then
     echo "rls-server seems to be installed already"
     exit 0
 fi
 
 
+local bin=1
+local man=1
+local src=1
+local etc=1
+local missing=0
+
+
+# Check if bin directory exists
+if [ ! -d $bindir ]; then
+    bin=0
+    missing=1
+fi
+
+# Check if man directory exists
+if [ ! -d $mandir ]; then
+   man=0
+   missing=1
+fi
+
+# Check if src directory exists
+if [ ! -d $srcdir ]; then
+    src=0
+    missing=1
+fi
+
+#check if etc directory exists
+if [ ! -d $etcdir ]; then
+    etc=0
+    missing=1
+fi
+
+if [ $missing -eq 1 ]; then
+    echo "The following directories are going to be created:"
+    if [ $bin -eq 0 ]; then
+        echo "    $bindir"
+    fi
+    if [ $man -eq 0 ]; then
+        echo "    $mandir"
+    fi
+    if [ $src -eq 0 ]; then
+        echo "    $srcdir"
+    fi
+    if [ $etc -eq 0 ]; then
+        echo "    $etcdir"
+    fi
+    echo "Continue? (y/n)"
+    
+    read -q ans
+    if [ "$ans" != "y" ]; then
+        echo "Installation aborted"
+        exit 0
+    fi
+
+    if [ $bin -eq 0 ]; then
+        mkdir -p $bindir
+        if [ $? -eq 0 ]; then
+            echo "Created $bindir"
+        else
+            echo "Failed to create $bindir"
+            exit 1
+        fi
+    fi
+    if [ $man -eq 0 ]; then
+        mkdir -p $mandir
+        if [ $? -eq 0 ]; then
+            echo "Created $mandir"
+        else
+            echo "Failed to create $mandir"
+            exit 1
+        fi
+    fi
+    if [ $src -eq 0 ]; then
+        mkdir -p $srcdir
+        if [ $? -eq 0 ]; then
+            echo "Created $srcdir"
+        else
+            echo "Failed to create $srcdir"
+            exit 1
+        fi
+    fi
+    if [ $etc -eq 0 ]; then
+        mkdir -p $etcdir
+        if [ $? -eq 0 ]; then
+            echo "Created $etcdir"
+        else
+            echo "Failed to create $etcdir"
+            exit 1
+        fi
+    fi
+fi
+    
+
+
 # Compile rls-server
+echo ""
+echo "Compiling rls-server ..."
 make
 if [ $? -ne 0 ]; then
     echo "Failed to compile rls-server"
     exit 1
 fi
+echo "... done"
+echo ""
 
-# Install rls-server in /usr/local/bin
-mv rls-server /usr/local/bin
+# Install rls-server in bin directory
+mv rls-server $bindir
 if [ $? -eq 0 ]; then
-    echo "Installed rls-server to /usr/local/bin"
+    echo "Installed rls-server to $bindir"
 else
-    echo "Failed to install rls-server to /usr/local/bin"
+    echo "Failed to install rls-server to $bindir"
     exit 1
 fi
 
@@ -64,10 +154,10 @@ fi
 
 
 # Create rls-server directory
-mkdir -p /etc/rls-server
+mkdir -p $etcdir/rls-server
 
 if [ $? -eq 0 ]; then
-    echo "Created rls-server directory under /etc"
+    echo "Created rls-server directory under $etcdir"
 else
     echo "Failed to create rls-server directory"
     exit 1
@@ -75,20 +165,20 @@ fi
 
 
 # Create rls-server default configuration file
-touch /etc/rls-server/rls-server.conf
+touch $etcdir/rls-server/rls-server.conf
 if [ $? -eq 0 ]; then
-    echo "Created rls-server configuration file under /etc/rls-server"
+    echo "Created rls-server configuration file under $etcdir/rls-server"
 else
     echo "Failed to create rls-server configuration file"
     exit 1
 fi
 
 # Initialize rls-server configuration file
-echo "SRVPORT=513" > /etc/rls-server/rls-server.conf
-echo "MAXCONN=16" >> /etc/rls-server/rls-server.conf
-echo "WPATTPT=3" >> /etc/rls-server/rls-server.conf
-echo "WPDELAY=2" >> /etc/rls-server/rls-server.conf
-echo "CONNTMO=10" >> /etc/rls-server/rls-server.conf
+echo "SRVPORT=513" > $etcdir/rls-server/rls-server.conf
+echo "MAXCONN=16" >> $etcdir/rls-server/rls-server.conf
+echo "WPATTPT=3" >> $etcdir/rls-server/rls-server.conf
+echo "WPDELAY=2" >> $etcdir/rls-server/rls-server.conf
+echo "CONNTMO=10" >> $etcdir/rls-server/rls-server.conf
 
 if [ $? -eq 0 ]; then
     echo "Initialized rls-server configuration file"
@@ -97,43 +187,53 @@ else
     exit 1
 fi
 
-# Move rls-server(1) to /usr/local/share/man/man1
-if [ ! -d "/usr/local/share/man/man1" ]; then
-    mkdir /usr/local/share/man/man1
+# Move rls-server(1) to man directory
+if [ ! -d "$mandir/man1" ]; then
+    mkdir $mandir/man1
     if [ $? -eq 0 ]; then
-        echo "Created /usr/local/share/man/man1 directory"
+        echo "Created $mandir/man1 directory"
     else
-        echo "Failed to create /usr/local/share/man/man1 directory"
+        echo "Failed to create $mandir/man1 directory"
         exit 1
     fi
 fi
-cp man/rls-server.1 /usr/local/share/man/man1
+cp man/rls-server.1 $mandir/man1
 if [ $? -eq 0 ]; then
-    echo "Moved rls-server(1) to /usr/local/share/man/man1"
+    echo "Moved rls-server(1) to $mandir/man1"
 else
-    echo "Failed to move rls-server(1) to /usr/local/share/man/man1"
+    echo "Failed to move rls-server(1) to $mandir/man1"
     exit 1
 fi
 
 # Move rls-server(7) to /usr/local/share/man/man7
-if [ ! -d "/usr/local/share/man/man7" ]; then
-    mkdir /usr/local/share/man/man7
+if [ ! -d "$mandir/man7" ]; then
+    mkdir $mandir/man7
     if [ $? -eq 0 ]; then
-        echo "Created /usr/local/share/man/man7 directory"
+        echo "Created $mandir/man7 directory"
     else
-        echo "Failed to create /usr/local/share/man/man7 directory"
+        echo "Failed to create $mandir/man7 directory"
         exit 1
     fi
 fi
-cp man/rls-server.7 /usr/local/share/man/man7
+cp man/rls-server.7 $mandir/man7
 if [ $? -eq 0 ]; then
-    echo "Moved rls-server(7) to /usr/local/share/man/man7"
+    echo "Moved rls-server(7) to $mandir/man7"
 else
-    echo "Failed to move rls-server(7) to /usr/local/share/man/man7"
+    echo "Failed to move rls-server(7) to $mandir/man7"
+    exit 1
+fi
+
+# Copy source code to src directory
+cp -r include src $srcdir/rls-server
+if [ $? -eq 0 ]; then
+    echo "Copied source code to $srcdir/rls-server"
+else
+    echo "Failed to copy source code to $srcdir/rls-server"
     exit 1
 fi
 
 
+echo ""
 echo "rls-server has been installed successfully"
 echo ""
 echo "    Default port: 513"
--- uninstall.sh
+++ uninstall.sh
@@ -8,38 +8,53 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 
+local bindir="/usr/local/bin"
+local mandir="/usr/local/share/man"
+local srcdir="/usr/local/src"
+local etcdir="/usr/local/etc"
+
+
 # Check if rls-server is installed
-if [ ! -f "/usr/local/bin/rls-server" ]; then
+if [ ! -f "$bindir/rls-server" ]; then
     echo "rls-server seems not to be installed"
     exit 0
 fi
 
 
-# Remove rls-server from /usr/local/bin
-rm /usr/local/bin/rls-server
+# Remove rls-server from bin directory
+rm $bindir/rls-server
 if [ $? -eq 0 ]; then
-    echo "Removed rls-server from /usr/local/bin"
+    echo "Removed rls-server from $bindir"
 else
-    echo "Cannot remove rls-server from /usr/local/bin"
+    echo "Cannot remove rls-server from $bindir"
     exit 1
 fi
 
 
 # Remove rls-server directory
-rm -rf /etc/rls-server
+rm -rf $etcdir/rls-server
 if [ $? -eq 0 ]; then
-    echo "Removed rls-server directory under /etc"
+    echo "Removed rls-server directory under $etcdir"
 else
-    echo "Cannot remove rls-server directory under /etc"
+    echo "Cannot remove rls-server directory under $etcdir"
     exit 1
 fi
 
 # Remove rls-server man pages
-rm -f /usr/local/share/man/man1/rls-server.1 /usr/local/share/man/man7/rls-server.7
+rm -f $mandir/man1/rls-server.1 $mandir/man7/rls-server.7
 if [ $? -eq 0 ]; then
-    echo "Removed rls-server man pages under /usr/local/man"
+    echo "Removed rls-server man pages under $mandir"
 else
-    echo "Cannot remove rls-server man pages under /usr/local/man"
+    echo "Cannot remove rls-server man pages under $mandir"
+    exit 1
+fi
+
+# Remove rls-server source code
+rm -rf $srcdir/rls-server
+if [ $? -eq 0 ]; then
+    echo "Removed rls-server source code under $srcdir"
+else
+    echo "Cannot remove rls-server source code under $srcdir"
     exit 1
 fi
 
